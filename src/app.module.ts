ef7ce29af240e8a8f1582762462010786fcb0802
iZeevens <izeevens@gmail.com>
2024-11-09 08:57:29 +0500

    refactor: create diff modules for each service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,17 @@
 import { Module } from '@nestjs/common';
-import UsersController from './users/users.controller';
-import UsersService from './users/users.service';
-import ArtistsController from './artists/artists.controller';
-import ArtistsService from './artists/artists.service';
-import TracksController from './tracks/tracks.controller';
-import TracksService from './tracks/tracks.service';
-import AlbumsController from './albums/albums.controller';
-import AlbumsService from './albums/albums.service';
-import FavoritesController from './favorites/favorites.controller';
-import FavoritesService from './favorites/favorites.service';
+import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
+import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    UsersController,
-    ArtistsController,
-    TracksController,
-    AlbumsController,
-    FavoritesController,
-  ],
-  providers: [
-    UsersService,
-    ArtistsService,
-    TracksService,
-    AlbumsService,
-    FavoritesService,
+  imports: [
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
   ],
 })
 export class AppModule {}
