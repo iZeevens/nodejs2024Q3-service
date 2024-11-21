6a455179eb33d8cadfe10cc7fbcfd4ca94ac43c7
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-11-21 20:29:40 +0500

    feat: add logs to file

--- src/common/logging/logging.service.ts
+++ src/common/logging/logging.service.ts
@@ -1,16 +1,29 @@
 import { ConsoleLogger, Injectable, LoggerService } from '@nestjs/common';
+import { promises as fs } from 'fs';
 
 @Injectable()
 export class CustomLogger extends ConsoleLogger implements LoggerService {
-  log(message: any, ...optionalParams: any[]) {
+  async log(message: any, ...optionalParams: any[]) {
     super.log(message, ...optionalParams);
+    await this.writeFile(message, 'LOG');
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  async error(message: any, ...optionalParams: any[]) {
     super.warn(message, ...optionalParams);
+    await this.writeFile(message, 'ERROR');
   }
 
-  warn(message: any, ...optionalParams: any[]) {
+  async warn(message: any, ...optionalParams: any[]) {
     super.warn(message, ...optionalParams);
+    await this.writeFile(message, 'WARN');
+  }
+
+  private async writeFile(message: string, type: 'LOG' | 'ERROR' | 'WARN') {
+    const directoryPath = `./loggerData.txt`;
+    const logMessage = `[${new Date().toISOString()}] [${type}] ${message}\n`;
+
+    await fs.appendFile(directoryPath, logMessage, {
+      encoding: 'utf8',
+    });
   }
 }
