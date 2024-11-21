81bed7c0222d18debc8cf0ea8cc535c764611562
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-11-21 20:57:49 +0500

    feat: add rotate logs

--- src/common/logging/logging.service.ts
+++ src/common/logging/logging.service.ts
@@ -1,5 +1,7 @@
 import { ConsoleLogger, Injectable, LoggerService } from '@nestjs/common';
 import { promises as fs } from 'fs';
+import { stat } from 'fs/promises';
+import { join } from 'path';
 
 @Injectable()
 export class CustomLogger extends ConsoleLogger implements LoggerService {
@@ -18,11 +20,35 @@ export class CustomLogger extends ConsoleLogger implements LoggerService {
     await this.writeFile(message, 'WARN');
   }
 
+  private async rotateFile(
+    filePath: string,
+    directoryPath: string,
+    type: 'LOG' | 'ERROR' | 'WARN',
+  ) {
+    try {
+      const stats = await stat(filePath);
+      const maxFileSize = 1 * 1024 * 1024;
+
+      if (stats.size >= maxFileSize) {
+        const timestamp = new Date().toISOString().replace(/:/g, '-');
+        const rotatedFileName = `${type}-${timestamp}.txt`;
+        const rotatedFilePath = join(directoryPath, rotatedFileName);
+
+        await fs.rename(filePath, rotatedFilePath);
+      }
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        super.error(`Error checking log file size: ${error.message}`);
+      }
+    }
+  }
+
   private async writeFile(message: string, type: 'LOG' | 'ERROR' | 'WARN') {
     const directoryPath = `./loggerData`;
-    const filePath = `${directoryPath}/${type}.txt`;
+    const filePath = join(directoryPath, `${type}.txt`);
     const logMessage = `[${new Date().toISOString()}] [${type}] ${message}\n`;
 
+    await this.rotateFile(filePath, directoryPath, type);
     await fs.mkdir(directoryPath, { recursive: true });
     await fs.appendFile(filePath, logMessage, {
       encoding: 'utf8',
